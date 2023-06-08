89ddccdbd24171c37a18a2e1431f8febe0202fa9
MarekAniol <marekaniol1@gmail.com>
2023-06-08 23:05:39 +0200

    added freezed models

--- lib/domain/models/quiz_question_model.dart
+++ lib/domain/models/quiz_question_model.dart
@@ -1,19 +1,43 @@
-class QuizQuestionModel {
-  const QuizQuestionModel({
-    required this.question,
-    required this.answers,
-    required this.correctAnswer,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'quiz_question_model.freezed.dart';
+
+// class QuizQuestionModel {
+//   const QuizQuestionModel({
+//     required this.question,
+//     required this.answers,
+//     required this.correctAnswer,
+//   });
+
+//   final String question;
+//   final List<String> answers;
+//   final String correctAnswer;
+
+//   factory QuizQuestionModel.fromJson(Map<String, dynamic> json) {
+//     return QuizQuestionModel(
+//       question: json['question'] as String,
+//       answers: json['answers'].cast<String>(),
+//       correctAnswer: json['correctAnswer'] as String,
+//     );
+//   }
+// }
+
+@freezed
+class QuizQuestionModel with _$QuizQuestionModel {
+  factory QuizQuestionModel({
+    required String question,
+    required List<String> answers,
+    required String correctAnswer,
+  }) = _QuizQuestionModel;
 
   final String question;
   final List<String> answers;
   final String correctAnswer;
 
-  factory QuizQuestionModel.fromJson(Map<String, dynamic> json) {
-    return QuizQuestionModel(
-      question: json['question'] as String,
-      answers: json['answers'].cast<String>(),
-      correctAnswer: json['correctAnswer'] as String,
-    );
-  }
+  const QuizQuestionModel._();
+
+  factory QuizQuestionModel.initial() => QuizQuestionModel(
+        question: '',
+        answers: [],
+        correctAnswer: '',
+      );
 }
